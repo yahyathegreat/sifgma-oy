62ddc32ea00dc33a617ef9daa308fe7983e03ead
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-29 15:06:51 +0500

    bird is ready

--- main.py
+++ main.py
@@ -7,4 +7,23 @@ class Bus(Vehicle):
     pass
 School_bus = Bus("School Volvo", 180, 12)
 print("Vehicle Name:", School_bus.name, "Speed", School_bus.max_speed,"Mileage:", School_bus.mileage)
-
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+        def whoisthis(self):
+            print("Bird")
+        def swim(self):
+            print("swim faster")
+class Penguin(Bird):
+    def __init__(self):
+        super().__init__()
+        super().__init__()
+        print("Penguin is ready")
+    def whoisthis(self):
+        print("Penguin")
+        def run(self):
+            print("Run faster")
+peggy = Penguin()
+peggy.whoisthis()
+peggy.swim()
+peggy.run()
